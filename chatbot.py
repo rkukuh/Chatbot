d046d2eca2f8d66675c52116791a564df2227af6
R. Kukuh <rkukuh@gmail.com>
2023-06-22 22:04:40 +0700

    Adding optional personalities

--- chatbot.py
+++ chatbot.py
@@ -1,26 +1,44 @@
 import openai
+import argparse
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 openai.api_key = config["OPENAI_API_KEY"]
 
-messages = []
+def main():
+    messages = []
 
-while True:
-    try:
-        user_input = input("You: ")
-        messages.append({"role": "user", "content": user_input})
+    parser = argparse.ArgumentParser(description="Simple command line with GPT")
 
-        response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages = messages
-        )
+    parser.add_argument("--personality", 
+                        type=str, 
+                        help="A brief summary of the chatbot's personality", 
+                        default="friendly and helpful")
+    
+    arguments = parser.parse_args()
 
-        messages.append(response["choices"][0]["message"].to_dict())
+    initial_prompt = f"You are a conversational chatbot. Your personality is: {arguments.personality}"
 
-        print("Assistant: ", response["choices"][0]["message"]["content"])
+    messages = [{"role": "assistant", "content": initial_prompt}]
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+    while True:
+        try:
+            user_input = input("You: ")
+            messages.append({"role": "user", "content": user_input})
+
+            response = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages = messages
+            )
+
+            messages.append(response["choices"][0]["message"].to_dict())
+
+            print("Assistant: ", response["choices"][0]["message"]["content"])
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+if __name__ == "__main__":
+    main()
